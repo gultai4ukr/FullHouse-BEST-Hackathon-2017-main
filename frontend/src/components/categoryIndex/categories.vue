dd76d4556cf3f764e90a2ace6559dce60f38dc8e
andriisiuta <andriisiuta@gmail.com>
2017-10-08 11:22:03 +0300

    add categories

--- frontend/src/components/categoryIndex/categories.vue
+++ frontend/src/components/categoryIndex/categories.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
-  <div @click="getParam('?category=music')">Music</div>
-  <div @click="getParam('?category=sports')">Sport</div>
-  <div v-for="item in this.$store.state.categoriesList">
+    <div @click="getParam('?category=music')">Music</div>
+    <div @click="getParam('?category=sports')">Sport</div>
+    <div @click="getParam('?category=comedy')">Comedy</div>
+    <div @click="getParam('?category=family_fun_kids')">familyFun</div>
+    <div @click="getParam('?category=books')">Books</div>
+    <div @click="getParam('?category=attractions')">attractions</div>
+    <div @click="getParam('?category=technology')">technology</div>
+    <div @click="getParam('?category=business')">business</div>
+    <div @click="getParam('?category=community')">community</div>
+    <div v-for="item in this.$store.state.categoriesList">
     <div>{{ item.title }}</div>
-    <div class="description">{{ item.description }}</div>
+    <div class="description" v-html=" item.description "></div>
   </div>
   </div>
 </template>
@@ -26,6 +33,9 @@
         this.$store.dispatch('searchCategory', param)
       }
     },
+    created: function () {
+      console.log(this.$route.param)
+    }
   }
 </script>
 
